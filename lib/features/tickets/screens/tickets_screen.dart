3276b8914e009df96bd12b31cec477d473b66ac8
rakib <rakibk55w@gmail.com>
2025-06-19 03:55:48 +0600

    Completed the UI section of ticket screen

--- lib/features/tickets/screens/tickets_screen.dart
+++ lib/features/tickets/screens/tickets_screen.dart
@@ -12,17 +12,23 @@ class TicketsScreen extends StatelessWidget {
     return Scaffold(
       appBar: const CustomAppbar(
         title: Text('M360ICT'),
-        actions: [
-          NotificationWithCounter(),
-        ],
+        actions: [NotificationWithCounter()],
       ),
-      body: Column(
-        children: [
-          const TicketsDashboard(),
-          const TicketView(),
-        ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            const TicketsDashboard(),
+            ListView.separated(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, __) => TicketView(),
+              separatorBuilder: (context, __) => SizedBox(height: 10),
+              itemCount: 8,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
