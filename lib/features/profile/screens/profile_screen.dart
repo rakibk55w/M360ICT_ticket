488a0fb6bf0681bc247b26c8e9fdff7e42c5d495
rakib <rakibk55w@gmail.com>
2025-06-20 01:56:30 +0600

    Added user details in profile screen

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -12,13 +12,47 @@ class ProfileScreen extends StatelessWidget {
       /// - Appbar
       appBar: const CustomAppbar(title: Text('My Profile')),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           /// - Profile tile
-          ProfileTile(
+          const ProfileTile(
             profileName: 'Jonaus Kahnwald',
             profileRole: 'Support',
             profileImage: 'assets/images/elon_musk.jpg',
           ),
+
+          /// - Basic info heading text
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: const Text('Basic Info',),
+          ),
+
+          /// - User details
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('First Name'),
+                Text('Jonaus'),
+                SizedBox(height: 8,),
+                Text('Last Name'),
+                Text('Kahnwald'),
+                SizedBox(height: 8,),
+                Text('Email'),
+                Text('username@email.com'),
+              ],
+            ),
+          ),
+
+          /// - Basic info heading text
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: const Text('Assigned roles (3)',),
+          ),
+
+          /// - List of roles
+
         ],
       ),
     );
