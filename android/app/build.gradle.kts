84650d5c128a9e40d7d1178417005e59d4afd6ab
rakib <rakibk55w@gmail.com>
2025-06-21 02:12:59 +0600

    Updated NDK version to mitigate version conflict

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.m360ict"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
