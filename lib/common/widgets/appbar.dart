fc7326fcbdf2147ea4d975677635dca9d3d7d788
rakib <rakibk55w@gmail.com>
2025-06-21 02:13:40 +0600

    Fixed an issue where scrolling cause appbar to dim

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -17,6 +17,7 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      forceMaterialTransparency: true,
       automaticallyImplyLeading: false,
       title: title,
       actions: actions,
